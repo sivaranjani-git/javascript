4582258eb64c0af0b848052319802d365ca7c7e9
sivaranjani-git <134845313+sivaranjani-git@users.noreply.github.com>
2023-08-14 16:31:10 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,185 @@
+//DATATYPES
+//NUMBER
+
+var number = 2; //integer
+var num = 2.5; //float
+console.log(number);
+console.log(num);
+var num1 = Infinity;
+console.log(num1);
+console.log("abc/2");
+
+//null data type
+let num4 = null;
+console.log(num4);
+
+//undefined
+var value;
+console.log(value);
+
+//string
+var string = "abcdef";
+var string1 = "123456";
+console.log(string1);
+
+//Boolean
+var logic= true;
+var logic2= false; 
+console.log(logic2);
+
+//ARITHMETIC OPERATOR
+let x=5;
+let y=2;
+var z;
+
+console.log("x+y=", x + y);
+console.log("x+y=", x - y);
+console.log("x*y=", x * y);
+console.log("x/y=", x / y );
+console.log("x%y=", x % y );
+//increment
+console.log("++x=", ++x);
+console.log("x++=", x++);
+console.log(x);
+//decrement
+console.log("--x",--y); //6
+console.log("x--",x--);//6
+console.log(x);//5
+
+console.log("x**y", x ** y);//25
+
+//ASSIGNMENT OPERATORS
+
+x = y;
+console.log("value of x",x);//2
+
+console.log("Addition assignment",x += y);//x=4
+console.log("substraction assignment",(x -= y));//x=x-y  x=4-2=2
+console.log("multtiplication assignment", (x *= y));//x=x*y x=2*2=4
+console.log("division assignment",(x /= y));//4/2=2
+console.log("Remainder assignment",(x %= y));//0
+console.log("exponential assignment",(y **= x));
+
+
+//comparison operators
+
+let X = 4;
+let Y = 2;
+console.log(x == y);//false
+console.log=(x != y);//true
+
+let a="10";
+let b= 10
+
+let c =  7;
+let d =  6;
+
+//console.log(c > d);
+
+
+
+//logical operator
+
+//logical OR
+
+//input 1 input 2 output
+//true    true    true
+//true    false   true 
+//false   true    true
+//false   false   false 
+
+varx = 2;
+vary = 4;
+
+
+//logical AND--- &&
+
+//input 1 input 2 output
+//true    true    true
+//true    false   false
+//false   true    false
+//false   false   false
+
+
+
+
+//logical NOT   !
+
+
+
+//BITWISE OR
+
+//INPUT1  00000001 ---1
+//INPUT2  00000010 ---2
+//OUTPUT  00000011 ----3
+
+
+//BITWISE AND
+//INPUT1 00000001 ---1
+//INPUT2 00000010 ---2
+//OUTPUT 00000000 ---0
+
+
+
+//ternary operator
+
+//condition?"val1":"val2"
+5 > 2?console.log(true):console.log(false);
+
+//even or odd
+//number%2==0---even number
+
+var number= 1948895612;
+number%2===0?console.log("even number") : console.log("odd number")
+console.log(num);
+
+//concentration operator
+
+let num1=  5;
+let num=  15;
+console.log("the addition of two number is" +(num1  + num2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
